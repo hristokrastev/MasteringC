74ec5581e5f8a5e9b938248a3cb20d4c48c182f3
hristokrastev <swifty1009>
2017-02-04 22:19:41 +0200

    Additional exercises added.

---
+++ TransportCoursesNeeded/Transport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TransportCoursesNeeded
+{
+    class Transport
+    {
+        static void Main()
+        {
+
+            int totalPeople = int.Parse(Console.ReadLine());
+
+            int totalSpace = 24;
+
+            int coursesNeeded = (int)Math.Ceiling(((double)totalPeople / totalSpace));
+
+            Console.WriteLine(coursesNeeded);
+        }
+    }
+}
