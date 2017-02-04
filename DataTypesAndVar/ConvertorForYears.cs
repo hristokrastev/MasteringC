f9151110d96ef9507c968666ec3ebc8d89768474
hristokrastev <swifty1009>
2017-02-04 20:27:30 +0200

    Adding ConvertorForYears on C#.

---
+++ DataTypesAndVar/ConvertorForYears.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataTypesAndVar
+{
+    class ConvertorForYears
+    {
+        static void Main()
+        {
+            int years = int.Parse(Console.ReadLine());
+
+            int days = years * 365;
+            int hours = days * 24;
+            long minutes = hours * 60;
+
+            Console.WriteLine($"{years} years = {days} days = {hours} hours = {minutes} minutes.");
+
+        }
+    }
+}
