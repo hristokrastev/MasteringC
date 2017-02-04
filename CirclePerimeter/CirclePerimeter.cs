375f90dae5cf7e9ff035e95bfd0d3f7ef2489cd2
hristokrastev <swifty1009>
2017-02-04 22:06:50 +0200

    Adding addition practicing projects.

---
+++ CirclePerimeter/CirclePerimeter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CirclePerimeter
+{
+    class CirclePerimeter
+    {
+        static void Main()
+        {
+
+            double r = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:f12}", 2 * Math.PI * r);
+        }
+    }
+}
---
+++ ExactProductOfTwoNumbers/ExactProductOfTwoNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ExactProductOfTwoNumbers
+{
+    class ExactProductOfTwoNumbers
+    {
+        static void Main()
+        {
+            int numbersOfProducts = int.Parse(Console.ReadLine());
+            decimal result = 1M;
+            for (int i = 1; i <= numbersOfProducts; i++)
+            {
+                result *= decimal.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
