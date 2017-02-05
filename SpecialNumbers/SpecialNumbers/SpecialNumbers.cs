13f8a71a079de04dca6d84fef2afb1eadcb04bf7
hristokrastev <swifty1009>
2017-02-05 13:25:50 +0200

    Few more ex. done.

---
+++ SpecialNumbers/SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SpecialNumbers
+{
+    class SpecialNumbers
+    {
+        static void Main()
+        {
+            int allNumbers = int.Parse(Console.ReadLine());
+
+            
+            for (int num= 0; num <= allNumbers; num++)
+            {
+                int sumOfDigits = 0;
+                int digit = num;
+               
+                
+                while (digit > 0)
+                {
+                    sumOfDigits += digit % 10;
+                    digit = digit / 10;
+                }
+                bool special = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+                Console.WriteLine($"{num} -> {special}");
+            }
+            
+        }
+    }
+}
---
+++ TripleOfLatinLetters/TripleOfLatinLetters/TripleOfLatinLetters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TripleOfLatinLetters
+{
+    class TripleOfLatinLetters
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            
+            for (int i = 0; i <= n - 1; i++)
+            {
+                for (int j = 0; j <= n - 1; j++)
+                {
+                    for (int k = 0; k <= n - 1; k++)
+                    {
+                        char letter1 = (char)('a' + i);
+                        char letter2 = (char)('a' + j);
+                        char letter3 = (char)('a' + k);
+                        Console.WriteLine($"{letter1}{letter2}{letter3}");
+                    }
+                }
+
+            }
+        }
+    }
+}
