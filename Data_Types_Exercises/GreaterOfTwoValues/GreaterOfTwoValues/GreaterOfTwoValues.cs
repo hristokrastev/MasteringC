5d0ab5a9a22f016c8f3b683081b10723d432d686
hristokrastev <swifty1009>
2017-02-20 16:23:34 +0200

    Finishing methods lab.

---
+++ Data_Types_Exercises/GreaterOfTwoValues/GreaterOfTwoValues/GreaterOfTwoValues.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace GreaterOfTwoValues
+{
+    class GreaterOfTwoValues
+    {
+        static void Main()
+        {
+            string type = Console.ReadLine();
+            if (type.Equals("int"))
+            {
+                int inputOne = int.Parse(Console.ReadLine());
+                int inputTwo = int.Parse(Console.ReadLine());
+                int max = GetMax(inputOne, inputTwo);
+                Console.WriteLine(max);
+            }
+            else if (type == "char")
+            {
+                char inputOne = char.Parse(Console.ReadLine());
+                char inputTwo = char.Parse(Console.ReadLine());
+                char max = GetMax(inputOne, inputTwo);
+                Console.WriteLine(max);
+            }
+            else if (type == "string")
+            {
+                string inputOne = Console.ReadLine();
+                string inputTwo = Console.ReadLine();
+                string max = GetMax(inputOne, inputTwo);
+                Console.WriteLine(max);
+            }                  
+        }
+
+        static string GetMax(string inputOne, string inputTwo)
+        {
+
+            if (inputOne.CompareTo(inputTwo) >= 0)
+            {
+                return inputOne;
+            }
+            else
+                return inputTwo;
+
+        }
+        static char GetMax(char inputOne, char inputTwo)
+        {
+            if (inputOne >= inputTwo)
+            {
+                return inputTwo;
+            }
+            else
+                return inputTwo;
+        }
+        static int GetMax(int inputOne, int inputTwo)
+        {
+            if (inputOne >= inputTwo)
+            {
+                return inputOne;
+            }
+            else
+                return inputTwo;
+        }
+        
+    }
+}
---
+++ Data_Types_Exercises/MathPower/MathPower/MathPower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MathPower
+{
+    class MathPower
+    {
+        static void Main()
+        {
+            var number = double.Parse(Console.ReadLine());
+            var power = int.Parse(Console.ReadLine());
+           
+            Console.WriteLine(poweredNumber(number, power));
+        }
+        static double poweredNumber(double number, int power)
+        {
+            return Math.Pow(number, power);
+        }
+    }
+}
